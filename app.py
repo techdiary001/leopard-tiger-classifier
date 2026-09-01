a9e9ba347026bd801c417b1278c05a084f296989
techdiary001 <dartyutibeima@gmail.com>
2026-09-01 13:02:22 +0100

    Modify model loading to include SafeInputLayer
    Added SafeInputLayer as a custom object for model loading.

--- app.py
+++ app.py
@@ -8,8 +8,18 @@ st.set_page_config(page_title="Leopard vs Tiger Classifier", page_icon="🐅", l
 
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model('leopard_tiger_model.h5', compile=False)
+    from tensorflow.keras.layers import InputLayer
+    class SafeInputLayer(InputLayer):
+        def __init__(self, *args, **kwargs):
+            kwargs.pop('batch_shape', None)
+            kwargs.pop('optional', None)
+            super().__init__(*args, **kwargs)
 
+    return tf.keras.models.load_model(
+        'leopard_tiger_model.h5', 
+        compile=False, 
+        custom_objects={'InputLayer': SafeInputLayer}
+    )
 model = load_model()
 
 st.title("🐅 Leopard vs Tiger Classifier 🐆")
