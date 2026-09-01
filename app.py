054228b025342f989ee4a33cb8850b9842bffea9
techdiary001 <dartyutibeima@gmail.com>
2026-09-01 13:59:42 +0100

    Fix indentation for image display in app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image_display = Image.open(uploaded_file)
-   st.image(image_display, caption='Uploaded Image', use_container_width=True)
+    st.image(image_display, caption='Uploaded Image', use_container_width=True)
 
     img = image_display.resize((150, 150))
     img_array = image.img_to_array(img)
